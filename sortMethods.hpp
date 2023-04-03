6c6d9a88016f931c66dca2ff697e23665afdd9f5
la1ki4 <100294709+la1ki4@users.noreply.github.com>
2023-04-03 21:53:05 +0300

    Add files via upload

---
+++ sortMethods.hpp
@@ -0,0 +1,11 @@
+#ifndef SORT_METHODS_
+#include "ParticipantOfTheCompetition.hpp"
+#include <iostream>
+#include <vector>
+
+
+void bubbleSort(std::vector<ParticipantsOfTheCompetition>&);
+void insertionSort(std::vector<ParticipantsOfTheCompetition>&);
+void selectionSort(std::vector<ParticipantsOfTheCompetition>&);
+
+#endif //SORT_METHODS_
